6e22f6d34b434ca39fc53407f3b715321bd7a11f
Cory <cory.goates@aggiemail.usu.edu>
2018-09-17 08:41:58 -0600

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   Database/components.db
    	modified:   Database/propellerCurves.py
    
     Untracked files:
    	Database/Props/sample perf calc.xls
    	Database/__pycache__/
    	Misc/kyosho_10x6 3rd of 3rd.png

--- Database/propellerCurves.py
+++ Database/propellerCurves.py
@@ -11,7 +11,7 @@ import re
 
 #########################################
 #Set this bool to determine if the SQL database will be updated or not
-updateDatabase = False
+updateDatabase = True
 #########################################
 
 propDatabasePath = os.getcwd() + "/Props"
@@ -20,18 +20,20 @@ propDatabasePath = os.getcwd() + "/Props"
 wholeDatabase = os.listdir(path.join(propDatabasePath))
 apcTestProps = ["apc_16x10", "apce_4x3.3", "apcr-rh_9x4.5"]
 seligTestProps = ["kyosho_10x6", "ance_8.5x6", "grcp_9x4", "kavfk_11x7.75", "mit_5x4", "rusp_11x4"]
+mixedProps = ["apc_16x10","apce_4x3.3","kyosho_10x6","grcp_9x4"]
 problemProps = ["da4022_9x6.75"]
+propSet = wholeDatabase
 
 thrustFitOrder = 3 #Order of polynomial fit to thrust vs advance ratio
 powerFitOrder = 3 #Order of polynomial fit to power vs advance ratio
 
-fitOfThrustFitOrder = 4 #Order of polynomial fit to thrust coefs vs rpm
-fitOfPowerFitOrder = 6 #Order of polynomial fit to power coefs vs rpm
+fitOfThrustFitOrder = 1 #Order of polynomial fit to thrust coefs vs rpm
+fitOfPowerFitOrder = 1 #Order of polynomial fit to power coefs vs rpm
 
-thrustZeroCoefs = [4] #Which polynomial fit coefficients should be set to zero for Selig's props
-powerZeroCoefs = [4,5,6]
+thrustZeroCoefs = [] #Which polynomial fit coefficients should be set to zero for Selig's props (for fitting coefs to RPM)
+powerZeroCoefs = []
 
-showPlots = False 
+showPlots = False
 propCount = 0
 
 if updateDatabase:
@@ -59,9 +61,11 @@ if updateDatabase:
         print(columnName)
     print(dbcur.fetchall())
 
-for propFolder in seligTestProps:
+for propFolder in propSet:
     
     if ".py" in str(propFolder):
+        continue
+    if ".xls" in str(propFolder):
         continue
     
     print("\n---", propFolder, "---")
@@ -144,108 +148,24 @@ for propFolder in seligTestProps:
         dataFile.close()
         
         #print("\nFit thrust coefficients to rpm")
-        fig = plt.figure(figsize=plt.figaspect(1.))
-        fig.suptitle(propFolder)
-        ax = fig.add_subplot(2, 2, 1)
-        
         fitOfThrustFit = np.zeros((thrustFitOrder+1, fitOfThrustFitOrder+1))
         
         for i in range(thrustFitOrder+1):
             fitOfThrustFit[i], r = fit.poly_fit(fitOfThrustFitOrder+1, rpms, thrustFitArray[:,i])
             #print(r**2)
-            rpmSpace = np.linspace(min(rpms), max(rpms), 100)
-            ax.plot(rpms, thrustFitArray[:,i], "o", markersize=4)
-            ax.plot(rpmSpace, fit.poly_func(fitOfThrustFit[i], rpmSpace), "-")
-        ax.set_title("Thrust " + str(thrustFitOrder) + "th Order Fit Coefficients")
-        ax.set_xlabel("RPM")
-        ax.set_ylabel("Magnitude of Coefficients")
-        ax.legend([b for a in (str(coef) for coef in range(thrustFitOrder+1)) for b in [a, a]])
         
         #print("\nFit power coefficients to rpm")
-        ax = fig.add_subplot(2, 2, 2)
-        
         fitOfPowerFit = np.zeros((powerFitOrder+1, fitOfPowerFitOrder+1))
+
         for i in range(powerFitOrder+1):
             fitOfPowerFit[i], r = fit.poly_fit(fitOfPowerFitOrder+1, rpms, powerFitArray[:,i])
             #print(r**2)
-            rpmSpace = np.linspace(min(rpms), max(rpms), 100)
-            ax.plot(rpms, powerFitArray[:,i], "o", markersize=4)
-            ax.plot(rpmSpace, fit.poly_func(fitOfPowerFit[i], rpmSpace), "-")
-        ax.set_title("Power " + str(powerFitOrder) + "th Order Fit Coefficients")
-        ax.set_xlabel("RPM")
-        ax.set_ylabel("Magnitude of Coefficients")
-        ax.legend([b for a in (str(coef) for coef in range(thrustFitOrder+1)) for b in [a, a]])
         
-        #print("\nThrust Prediction")
-        maxError = 0
-        ax = fig.add_subplot(2,2,3, projection='3d')
-        
-        for rpm in rpms:
-            a = fit.poly_func(fitOfThrustFit.T, rpm)
-            advRatioSpace = np.linspace(0, 1.2, 100)
-            thrust = fit.poly_func(a, advRatioSpace)
-            rpmExp = np.full(len(propCoefDict[rpm][:,0]), rpm)
-            rpmTheo = np.full(len(thrust), rpm)
-            ax.plot(propCoefDict[rpm][:,0], rpmExp, propCoefDict[rpm][:,2], "bo", markersize=4)
-            ax.plot(advRatioSpace, rpmTheo, thrust, "r-")
-            
-            SSE = 0
-            
-            for exp, theo in zip(propCoefDict[rpm][:,2], thrust):
-                SSE += (exp - theo)**2
-                if abs(exp - theo) > maxError:
-                    maxError = abs(exp - theo)
-            
-            RMSerror = np.sqrt(SSE/len(thrust))
-            #print("RMS error: ", RMSerror)
-            
-            a = fit.poly_func(fitOfThrustFit.T, rpm+500)
-            thrust = fit.poly_func(a, advRatioSpace)
-            ax.plot(advRatioSpace, rpmTheo+500, thrust, "g-")
-            
-        ax.set_title("Predicted thrust from " + str(fitOfThrustFitOrder) + "th order fit of " + str(thrustFitOrder) + "th order fit")
-        ax.set_xlabel("Advance Ratio")
-        ax.set_ylabel("RPM")
-        ax.set_zlabel("Thrust Coefficient")
         #print("Max Error: ", maxError)
-        
-        #print("\nPower Prediction")
-        maxError = 0
-        ax = fig.add_subplot(2,2,4, projection='3d')
-        
-        for rpm in rpms:
-            a = fit.poly_func(fitOfPowerFit.T, rpm)
-            advRatioSpace = np.linspace(0, 1.2, 100)
-            power = fit.poly_func(a, advRatioSpace)
-            rpmExp = np.full(len(propCoefDict[rpm][:,0]), rpm)
-            rpmTheo = np.full(len(power), rpm)
-            ax.plot(propCoefDict[rpm][:,0], rpmExp, propCoefDict[rpm][:,3], "bo", markersize=4)
-            ax.plot(advRatioSpace, rpmTheo, power, "r-")
-            
-            SSE = 0
-            
-            for exp, theo in zip(propCoefDict[rpm][:,3], power):
-                SSE += (exp - theo)**2
-                if abs(exp - theo) > maxError:
-                    maxError = abs(exp - theo)
-            
-            RMSerror = np.sqrt(SSE/len(power))
-            #print("RMS Error: ", RMSerror)
-            
-            a = fit.poly_func(fitOfPowerFit.T, rpm+500)
-            power = fit.poly_func(a, advRatioSpace)
-            ax.plot(advRatioSpace, rpmTheo+500, power, "g-")
-            
-        ax.set_title("Predicted Power from " + str(fitOfPowerFitOrder) + "th order fit of " + str(powerFitOrder) + "th order fit")
-        ax.set_xlabel("Advance Ratio")
-        ax.set_ylabel("RPM")
-        ax.set_zlabel("Power Coefficient")
-        #print("Max Error: ", maxError)
-        if showPlots:
-            plt.show()
         
         print("Thrust Fit:\n", fitOfThrustFit)
         print("Power Fit:\n", fitOfPowerFit)        
+    #----------------------------------END OF APC--------------------------------------------------
         
     elif dataType == "selig": #Data files from U of I U-C
         
@@ -383,129 +303,156 @@ for propFolder in seligTestProps:
             
         #Fit thrust and power fit coefficients to rpm
         #print("\nFit thrust coefficients to rpm")
-        fig = plt.figure(figsize=plt.figaspect(1.))
-        fig.suptitle(propFolder)
-        ax = fig.add_subplot(2, 2, 1)
-        
         fitOfThrustFit = np.zeros((thrustFitOrder+1, fitOfThrustFitOrder+1))
         
         for i in range(thrustFitOrder+1):
             fitOfThrustFit[i], r = fit.poly_fit(fitOfThrustFitOrder+1, rpms, thrustFitArray[:,i], forcezero=thrustZeroCoefs)
             #print(r**2)
-            rpmSpace = np.linspace(min(rpms), max(rpms), 100)
-            ax.plot(rpms, thrustFitArray[:,i], "o", markersize=4)
-            ax.plot(rpmSpace, fit.poly_func(fitOfThrustFit[i], rpmSpace), "-")
-        ax.set_title("Thrust " + str(thrustFitOrder) + "th Order Fit Coefficients")
-        ax.set_xlabel("RPM")
-        ax.set_ylabel("Magnitude of Coefficients")
-        ax.legend([b for a in (str(coef) for coef in range(thrustFitOrder+1)) for b in [a, a]])
         
         #print("\nFit power coefficients to rpm")
-        ax = fig.add_subplot(2, 2, 2)
-        
         fitOfPowerFit = np.zeros((powerFitOrder+1, fitOfPowerFitOrder+1))
+
         for i in range(powerFitOrder+1):
             fitOfPowerFit[i], r = fit.poly_fit(fitOfPowerFitOrder+1, rpms, powerFitArray[:,i], forcezero=powerZeroCoefs)
             #print(r**2)
-            rpmSpace = np.linspace(min(rpms), max(rpms), 100)
-            ax.plot(rpms, powerFitArray[:,i], "o", markersize=4)
-            ax.plot(rpmSpace, fit.poly_func(fitOfPowerFit[i], rpmSpace), "-")
-        ax.set_title("Power " + str(powerFitOrder) + "th Order Fit Coefficients")
-        ax.set_xlabel("RPM")
-        ax.set_ylabel("Magnitude of Coefficients")
-        ax.legend([b for a in (str(coef) for coef in range(thrustFitOrder+1)) for b in [a, a]])
+    #-----------------------END OF SELIG/UIUC---------------------------------------------
         
-        #print("\nThrust Prediction")
-        maxError = 0
-        ax = fig.add_subplot(2,2,3, projection='3d')
-        
-        for rpm in rpms:
-            a = fit.poly_func(fitOfThrustFit.T, rpm)
-            advRatioSpace = np.linspace(0, 1.2, 100)
-            thrust = fit.poly_func(a, advRatioSpace)
-            rpmExp = np.full(len(propCoefDict[rpm][:,0]), rpm)
-            rpmTheo = np.full(len(thrust), rpm)
-            ax.plot(propCoefDict[rpm][:,0], rpmExp, propCoefDict[rpm][:,2], "bo", markersize=4)
-            ax.plot(advRatioSpace, rpmTheo, thrust, "r-")
-            
-            SSE = 0
-            
-            for exp, theo in zip(propCoefDict[rpm][:,2], thrust):
-                SSE += (exp - theo)**2
-                if abs(exp - theo) > maxError:
-                    maxError = abs(exp - theo)
-            
-            RMSerror = np.sqrt(SSE/len(thrust))
-            #print("RMS error: ", RMSerror)
-            
-            a = fit.poly_func(fitOfThrustFit.T, rpm+500)
-            thrust = fit.poly_func(a, advRatioSpace)
-            plt.plot(advRatioSpace, rpmTheo+500, thrust, "g-")
-            
-        ax.set_title("Predicted thrust from " + str(fitOfThrustFitOrder) + "th order fit of " + str(thrustFitOrder) + "th order fit")
-        ax.set_xlabel("Advance Ratio")
-        ax.set_ylabel("RPM")
-        ax.set_zlabel("Thrust Coefficient")
-        #print("Max Error: ", maxError)
-        
-        #print("\nPower Prediction")
-        maxError = 0
-        ax = fig.add_subplot(2,2,4, projection='3d')
-        
-        for rpm in rpms:
-            a = fit.poly_func(fitOfPowerFit.T, rpm)
-            advRatioSpace = np.linspace(0, 1.2, 100)
-            power = fit.poly_func(a, advRatioSpace)
-            rpmExp = np.full(len(propCoefDict[rpm][:,0]), rpm)
-            rpmTheo = np.full(len(power), rpm)
-            ax.plot(propCoefDict[rpm][:,0], rpmExp, propCoefDict[rpm][:,3], "bo", markersize=4)
-            ax.plot(advRatioSpace, rpmTheo, power, "r-")
-            
-            SSE = 0
-            
-            for exp, theo in zip(propCoefDict[rpm][:,3], power):
-                SSE += (exp - theo)**2
-                if abs(exp - theo) > maxError:
-                    maxError = abs(exp - theo)
-            
-            RMSerror = np.sqrt(SSE/len(power))
-            #print("RMS Error: ", RMSerror)
-            
-            a = fit.poly_func(fitOfPowerFit.T, rpm+500)
-            power = fit.poly_func(a, advRatioSpace)
-            ax.plot(advRatioSpace, rpmTheo+500, power, "g-")
-            
-        ax.set_title("Predicted Power from " + str(fitOfPowerFitOrder) + "th order fit of " + str(powerFitOrder) + "th order fit")
-        ax.set_xlabel("Advance Ratio")
-        ax.set_ylabel("RPM")
-        ax.set_zlabel("Power Coefficient")
-        #print("Max Error: ", maxError)
-        if showPlots:
-            plt.show()
-            
-        print("Thrust Fit:\n", fitOfThrustFit)
-        print("Power Fit:\n", fitOfPowerFit)
-    
-    #Store coefficients and geometry in the components.db database
-    insertCommand = "insert into props (Name, Diameter, Pitch) values (\""+str(propFolder)+"\","+str(diameter)+","+str(pitch)+")"
-    print(insertCommand)
-    dbcur.execute(insertCommand)
+    #Predict thrust and power based off of fits
+    #ALL FOLLOWING CODE IS EXECUTED FOR BOTH TYPES OF PROPS
+    #print("\nThrust Prediction")
+    maxError = 0
+    fig = plt.figure(figsize=plt.figaspect(1.))
+    fig.suptitle(propFolder)
 
+    #Fits of fits
+    rpmSpace = np.linspace(0,max(rpms),100)
+    ax = fig.add_subplot(2, 2, 1)
     for i in range(thrustFitOrder+1):
-        for j in range(fitOfThrustFitOrder+1):
-            insertCommand = "update props set thrust"+str(i)+str(j)+"="+str(fitOfThrustFit[i,j])+" where name = \""+str(propFolder)+"\""
-            print(insertCommand)
-            dbcur.execute(insertCommand)
+        ax.plot(rpms, thrustFitArray[:,i], "o", markersize=4)
+        ax.plot(rpmSpace, fit.poly_func(fitOfThrustFit[i], rpmSpace), "-")
+    ax.set_title("Thrust " + str(thrustFitOrder) + "th Order Fit Coefficients")
+    ax.set_xlabel("RPM")
+    ax.set_ylabel("Magnitude of Coefficients")
+    ax.legend([b for a in (str(coef) for coef in range(thrustFitOrder+1)) for b in [a, a]])
 
+
+    ax = fig.add_subplot(2, 2, 2)
     for i in range(powerFitOrder+1):
-        for j in range(fitOfPowerFitOrder+1):
-            insertCommand = "update props set power"+str(i)+str(j)+"="+str(fitOfPowerFit[i,j])+" where name = \""+str(propFolder)+"\""
-            print(insertCommand)
-            dbcur.execute(insertCommand)
+        ax.plot(rpms, powerFitArray[:,i], "o", markersize=4)
+        ax.plot(rpmSpace, fit.poly_func(fitOfPowerFit[i], rpmSpace), "-")
+    ax.set_title("Power " + str(powerFitOrder) + "th Order Fit Coefficients")
+    ax.set_xlabel("RPM")
+    ax.set_ylabel("Magnitude of Coefficients")
+    ax.legend([b for a in (str(coef) for coef in range(thrustFitOrder+1)) for b in [a, a]])
 
-print("Successfully analyzed and stored", propCount, "props.")
-dbcur.execute("select * from props")
-print(dbcur.fetchall())
+    #Fits
+    ax = fig.add_subplot(2,2,3, projection='3d')
 
-db.commit()
-db.close()
+    for rpm in rpms:
+        rpmExp = np.full(len(propCoefDict[rpm][:,0]), rpm)
+        ax.plot(propCoefDict[rpm][:,0], rpmExp, propCoefDict[rpm][:,2], "bo", markersize=4)
+
+        a = fit.poly_func(fitOfThrustFit.T, rpm)
+        advRatioSpace = np.linspace(0, 1.2, 100)
+        thrust = fit.poly_func(a, advRatioSpace)
+        rpmTheo = np.full(len(thrust), rpm)
+        ax.plot(advRatioSpace, rpmTheo, thrust, "r-")
+     
+        SSE = 0
+       
+        for exp, theo in zip(propCoefDict[rpm][:,2], thrust):
+            SSE += (exp - theo)**2
+            if abs(exp - theo) > maxError:
+                maxError = abs(exp - theo)
+            
+        RMSerror = np.sqrt(SSE/len(thrust))
+        #print("RMS error: ", RMSerror)
+           
+        a = fit.poly_func(fitOfThrustFit.T, rpm+500)
+        thrust = fit.poly_func(a, advRatioSpace)
+        plt.plot(advRatioSpace, rpmTheo+500, thrust, "r-")
+
+    for rpm in np.linspace(0,min(rpms),10): #Plot fits down to 0 RPMs to examine end behavior
+        rpmTheo = np.full(len(thrust), rpm)
+        a = fit.poly_func(fitOfThrustFit.T, rpm)
+        thrust = fit.poly_func(a, advRatioSpace)
+        plt.plot(advRatioSpace, rpmTheo, thrust, "g-")
+            
+    ax.set_title("Predicted thrust from " + str(fitOfThrustFitOrder) + "th order fit of " + str(thrustFitOrder) + "th order fit")
+    ax.set_xlabel("Advance Ratio")
+    ax.set_ylabel("RPM")
+    ax.set_zlabel("Thrust Coefficient")
+    #print("Max Error: ", maxError)
+        
+    #print("\nPower Prediction")
+    maxError = 0
+    ax = fig.add_subplot(2,2,4, projection='3d')
+    
+    for rpm in rpms:
+        a = fit.poly_func(fitOfPowerFit.T, rpm)
+        advRatioSpace = np.linspace(0, 1.2, 100)
+        power = fit.poly_func(a, advRatioSpace)
+        rpmExp = np.full(len(propCoefDict[rpm][:,0]), rpm)
+        rpmTheo = np.full(len(power), rpm)
+        ax.plot(propCoefDict[rpm][:,0], rpmExp, propCoefDict[rpm][:,3], "bo", markersize=4)
+        ax.plot(advRatioSpace, rpmTheo, power, "r-")
+         
+        SSE = 0
+       
+        for exp, theo in zip(propCoefDict[rpm][:,3], power):
+            SSE += (exp - theo)**2
+            if abs(exp - theo) > maxError:
+                maxError = abs(exp - theo)
+            
+        RMSerror = np.sqrt(SSE/len(power))
+        #print("RMS Error: ", RMSerror)
+        
+        a = fit.poly_func(fitOfPowerFit.T, rpm+500)
+        power = fit.poly_func(a, advRatioSpace)
+        ax.plot(advRatioSpace, rpmTheo+500, power, "r-")
+
+    for rpm in np.linspace(0,min(rpms),10): #Plot fits down to 0 RPMs to examine end behavior
+        rpmTheo = np.full(len(power), rpm)
+        a = fit.poly_func(fitOfPowerFit.T, rpm)
+        power = fit.poly_func(a, advRatioSpace)
+        ax.plot(advRatioSpace, rpmTheo, power, "g-")
+        
+            
+    ax.set_title("Predicted Power from " + str(fitOfPowerFitOrder) + "th order fit of " + str(powerFitOrder) + "th order fit")
+    ax.set_xlabel("Advance Ratio")
+    ax.set_ylabel("RPM")
+    ax.set_zlabel("Power Coefficient")
+    #print("Max Error: ", maxError)
+    if showPlots:
+        plt.show()
+           
+    print("Thrust Fit:\n", fitOfThrustFit)
+    print("Power Fit:\n", fitOfPowerFit)
+
+    if updateDatabase:
+    
+        #Store coefficients and geometry in the components.db database
+        insertCommand = "insert into props (Name, Diameter, Pitch) values (\""+str(propFolder)+"\","+str(diameter)+","+str(pitch)+")"
+        print(insertCommand)
+        dbcur.execute(insertCommand)
+
+        for i in range(thrustFitOrder+1):
+            for j in range(fitOfThrustFitOrder+1):
+                insertCommand = "update props set thrust"+str(i)+str(j)+"="+str(fitOfThrustFit[i,j])+" where name = \""+str(propFolder)+"\""
+                print(insertCommand)
+                dbcur.execute(insertCommand)
+
+        for i in range(powerFitOrder+1):
+            for j in range(fitOfPowerFitOrder+1):
+                insertCommand = "update props set power"+str(i)+str(j)+"="+str(fitOfPowerFit[i,j])+" where name = \""+str(propFolder)+"\""
+                print(insertCommand)
+                dbcur.execute(insertCommand)
+
+if updateDatabase:
+    dbcur.execute("select * from props")
+    print(dbcur.fetchall())
+    print("Successfully stored ", propCount, " props.")
+    db.commit()
+    db.close()
+
+print("Successfully analyzed ", propCount, " props.")
