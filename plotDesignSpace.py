6af254eab6e368a5ef8ad4bfabe1cfc4871208f1
Cory <cory.goates@aggiemail.usu.edu>
2018-10-31 15:46:11 -0600

    On branch master  Your branch is ahead of 'origin/master' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	new file:   Database/components.db
    	new file:   README.txt
    	new file:   __pycache__/polyFit.cpython-36.pyc
    	new file:   __pycache__/propulsionUnitClass.cpython-36.pyc
    	new file:   __pycache__/supportClasses.cpython-36.pyc
    	new file:   dev/Obsolete/propulsionUnitClass.py
    	new file:   plotDesignSpace.py
    	new file:   polyFit.py
    	new file:   supportClasses.py

---
+++ plotDesignSpace.py
@@ -0,0 +1,230 @@
+import matplotlib.pyplot as plt
+import sqlite3 as sql
+import supportClasses as s
+import numpy as np
+from random import randint,seed
+import multiprocessing as mp
+import math
+import sys
+import warnings
+from datetime import datetime
+
+dbFile = "Database/components.db"
+
+def printUnitInfo(unit):
+    print("----Propulsion Unit----")
+    print("Prop:",unit.prop.name)
+    print("Motor:",unit.motor.name)
+    print("      Kv:",unit.motor.Kv)
+    print("      I0:",unit.motor.I0,"A")
+    print("      R:",unit.motor.R,"ohms")
+    print("      weight:",unit.motor.weight,"oz")
+    print("ESC:",unit.esc.name)
+    print("      weight:",unit.esc.weight,"oz")
+    print("Battery:",unit.batt.name)
+    print("      capacity:",unit.batt.cellCap,"mAh")
+    print("      cells:",unit.batt.n)
+    print("      V:",unit.batt.V0,"V")
+    print("      weight:",unit.batt.weight,"oz")
+    print("Total weight:",unit.GetWeight()+W_frame,"lb")
+
+def on_pick(event):
+    artist = event.artist
+    xmouse,ymouse = event.mouseevent.xdata, event.mouseevent.ydata
+    x,y = artist.get_xdata(), artist.get_ydata()
+    ind = int(event.ind[0])
+    selUnit = units[ind]
+
+    axClicked = event.mouseevent.inaxes
+    fig = plt.figure(plt.get_fignums()[0])
+    fig.suptitle("SELCTED Prop: "+str(selUnit.prop.name)+"  Motor: "+str(selUnit.motor.name)+"  Battery: "+str(selUnit.batt.name)+"  ESC: "+str(selUnit.esc.name))
+    ax = fig.axes
+    ax[0].plot(selUnit.prop.diameter,t_flight[ind],'o')
+    ax[1].plot(selUnit.prop.pitch,t_flight[ind],'o')
+    ax[2].plot(selUnit.motor.Kv,t_flight[ind],'o')
+    ax[3].plot(selUnit.batt.V0,t_flight[ind],'o')
+    ax[4].plot(selUnit.batt.cellCap,t_flight[ind],'o')
+    ax[5].plot(selUnit.GetWeight()+W_frame,t_flight[ind],'o')
+    printUnitInfo(selUnit)
+    print("Flight Time:",t_flight[ind],"min")
+    if optimizeForRatio:
+        print("    at {:4.2f}% throttle".format(selUnit.CalcCruiseThrottle(v_req,(selUnit.GetWeight()+W_frame)*R_tw_req)*100))
+    else:
+        print("    at {:4.2f}% throttle".format(selUnit.CalcCruiseThrottle(v_req,T_req)*100))
+    selUnit.PlotThrustCurves(v_req*2+10,11,51)
+
+def setGlobalCursor():
+    global dbcur
+    dbcur = sql.connect(dbFile).cursor()
+
+def getCombination(args):
+
+    v_req = args[0]
+    T = args[1]
+    h = args[2]
+    optimizeForRatio = args[3]
+
+    if optimizeForRatio:
+        R_tw_req = T
+    else:
+        T_req = T
+
+    # Get numbers of components from database
+    dbcur.execute("select count(*) from Props")
+    N_props = int(dbcur.fetchall()[0][0])
+    dbcur.execute("select count(*) from Motors")
+    N_motors = int(dbcur.fetchall()[0][0])
+    dbcur.execute("select count(*) from Batteries")
+    N_batt = int(dbcur.fetchall()[0][0])
+    dbcur.execute("select count(*) from ESCs")
+    N_escs = int(dbcur.fetchall()[0][0])
+
+    seed(datetime.now())
+    
+    t_flight_curr = None
+    while t_flight_curr is None or math.isnan(t_flight_curr):
+
+        propID = randint(1,N_props)
+        motorID = randint(1,N_motors)
+        battID = randint(1,N_batt)
+        numCells = randint(2,5)
+        escID = randint(1,N_escs)
+
+        #Fetch prop data
+        formatString = """select * from Props where id = {ID}"""
+        command = formatString.format(ID = propID)
+        results = dbcur.execute(command)
+        propInfo = np.asarray(results.fetchone()).flatten()
+        prop = s.Propeller(propInfo[1],propInfo[2],propInfo[3],propInfo[4],propInfo[5:])
+
+        #Fetch motor data
+        formatString = """select * from Motors where id = {ID}"""
+        command = formatString.format(ID = motorID)
+        results = dbcur.execute(command)
+        motorInfo = np.asarray(results.fetchone()).flatten()
+        motor = s.Motor(motorInfo[1],motorInfo[2],motorInfo[3],motorInfo[4],motorInfo[6],motorInfo[5],motorInfo[7])
+
+        #Fetch ESC data
+        formatString = """select * from ESCs where id = {ID}"""
+        command = formatString.format(ID = escID)
+        results = dbcur.execute(command)
+        escInfo = np.asarray(results.fetchone()).flatten()
+        esc = s.ESC(escInfo[1],escInfo[2],escInfo[6],escInfo[3], escInfo[5])
+
+        #Fetch battery data
+        formatString = """select * from Batteries where id = {ID}"""
+        command = formatString.format(ID = battID)
+        results = dbcur.execute(command)
+        batteryInfo = np.asarray(results.fetchone()).flatten()
+        batt = s.Battery(batteryInfo[1],batteryInfo[2],numCells,batteryInfo[4],batteryInfo[7],batteryInfo[6],batteryInfo[5],batteryInfo[3])
+
+        if batt.R == 0 and esc.R == 0 and motor.R == 0:
+            continue
+
+        currUnit = s.PropulsionUnit(prop,motor,batt,esc,h)
+        if optimizeForRatio:
+            T_req = (currUnit.GetWeight()+args[4])*R_tw_req
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            t_flight_curr = currUnit.CalcBattLife(v_req,T_req)
+    return t_flight_curr, currUnit
+
+#----------------------BEGINNING OF COMPUTATION------------------------------------
+
+v_req = None
+T_req = None
+R_tw_req = None
+h = None
+W_frame = None
+N_units = 10000 #default value
+N_proc_max = 8 #default value
+args = sys.argv
+for i,arg in enumerate(args):
+    if "units" in arg:
+        N_units = int(args[i+1])
+    if "mp" in arg:
+        N_proc_max = int(args[i+1])
+    if "speed" in arg:
+        v_req = float(args[i+1])
+    if "thrust" in arg:
+        T_req = float(args[i+1])
+        W_frame = 0
+        optimizeForRatio = False
+    if "thrustToWeight" in arg:
+        R_tw_req = float(args[i+1])
+        optimizeForRatio = True
+    if "alt" in arg:
+        h = float(args[i+1])
+    if "weight" in arg:
+        W_frame = float(args[i+1])
+
+if v_req is None or (T_req is None and R_tw_req is None) or h is None:
+    raise ValueError('One or more required parameters were not specified')
+if optimizeForRatio and W_frame is None:
+    raise ValueError('Airframe weight not specified for thrust-to-weight analysis')
+
+if optimizeForRatio:
+    print("Optimizing for a thrust-to-weight ratio of",R_tw_req,"at a speed of",v_req,"ft/s and an altitude of",h,"ft")
+    thrustParam = R_tw_req
+else:
+    print("Optimizing for a thrust of",T_req,"lbf at a speed of",v_req,"ft/s and an altitude of",h)
+    thrustParam = T_req
+
+
+# Distribute work
+with mp.Pool(processes=N_proc_max,initializer=setGlobalCursor,initargs=()) as pool:
+    args = [(v_req,thrustParam,h,optimizeForRatio,W_frame) for i in range(N_units)]
+    data = pool.map(getCombination,args)
+sql.connect(dbFile).close()
+
+t_flight,units = map(list,zip(*data))
+
+# Determine optimum
+t_max = max(t_flight)
+bestUnit = units[t_flight.index(t_max)]
+
+print("Maximum flight time found:",t_max,"min")
+print("Prop:",bestUnit.prop.name)
+print("Motor:",bestUnit.motor.name,"(Kv =",bestUnit.motor.Kv,")")
+print("Battery:",bestUnit.batt.name,"(Capacity =",bestUnit.batt.cellCap,", Voltage =",bestUnit.batt.V0,")")
+print("ESC:",bestUnit.esc.name)
+print("Throttle setting for max flight:",bestUnit.CalcCruiseThrottle(v_req,T_req))
+print("Current draw:",bestUnit.Im,"A")
+
+# Plot design space
+plt.ion()
+fig,((ax1,ax2,ax3),(ax4,ax5,ax6)) = plt.subplots(nrows=2,ncols=3)
+fig.suptitle("OPTIMUM Prop: "+str(bestUnit.prop.name)+"  Motor: "+str(bestUnit.motor.name)+"  Battery: "+str(bestUnit.batt.name)+"  ESC: "+str(bestUnit.esc.name))
+ax1.plot([units[i].prop.diameter for i in range(N_units)],t_flight,'b*',picker=3)
+ax1.plot(bestUnit.prop.diameter,t_max,'r*')
+ax1.set_xlabel("Prop Diameter [in]")
+ax1.set_ylabel("Flight Time [min]")
+
+ax2.plot([units[i].prop.pitch for i in range(N_units)],t_flight,'b*',picker=3)
+ax2.plot(bestUnit.prop.pitch,t_max,'r*')
+ax2.set_xlabel("Prop Pitch [in]")
+ax2.set_ylabel("Flight Time [min]")
+
+ax3.plot([units[i].motor.Kv for i in range(N_units)],t_flight,'b*',picker=3)
+ax3.plot(bestUnit.motor.Kv,t_max,'r*')
+ax3.set_xlabel("Motor Kv [rpm/V]")
+ax3.set_ylabel("Flight Time [min]")
+
+ax4.plot([units[i].batt.V0 for i in range(N_units)],t_flight,'b*',picker=3)
+ax4.plot(bestUnit.batt.V0,t_max,'r*')
+ax4.set_xlabel("Battery Voltage [V]")
+ax4.set_ylabel("Flight Time [min]")
+
+ax5.plot([units[i].batt.cellCap for i in range(N_units)],t_flight,'b*',picker=3)
+ax5.plot(bestUnit.batt.cellCap,t_max,'r*')
+ax5.set_xlabel("Cell Capacity [mAh]")
+ax5.set_ylabel("Flight Time [min]")
+
+ax6.plot([units[i].GetWeight()+W_frame for i in range(N_units)],t_flight,'b*',picker=3)
+ax6.plot(bestUnit.GetWeight()+W_frame,t_max,'r*')
+ax6.set_xlabel("Total Unit Weight [lb]")
+ax6.set_ylabel("Flight Time [min]")
+
+fig.canvas.mpl_connect('pick_event',on_pick)
+plt.show(block=True)
+plt.ioff()
---
+++ supportClasses.py
@@ -0,0 +1,318 @@
+import numpy as np
+import copy
+from scipy import integrate
+import scipy.interpolate as interp
+import os
+from os import path
+import matplotlib.pyplot as plt
+import polyFit as fit
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from skaero.atmosphere import coesa
+
+#Converts rads per second to rpms
+def toRPM(rads):
+    return rads*30/np.pi
+    
+#A class that defines a battery
+class Battery:
+
+    #Initialize members from properties
+    def __init__(self, name, manu, numCells, capacity, voltage, resistance, weight, maxCurr):
+
+        #Define members from inputs
+        self.n = int(numCells)
+        self.cellCap = float(capacity)
+        self.cellV = float(voltage)
+        self.cellR = float(resistance)
+        self.name = name
+        self.manufacturer = manu
+        self.cellWeight = float(weight)
+        self.iMax = float(maxCurr)
+
+        #Members derived from inputs
+        self.V0 = self.cellV * self.n
+        self.R = self.cellR * self.n
+        self.weight = self.cellWeight*self.n
+
+#A class that defines an ESC (Electronic Speed Controller)
+class ESC:
+
+    #Initialization of the class from properties
+    def __init__(self, name, manu, resistance, iMax, weight):
+
+        self.R = float(resistance)
+        self.name = name
+        self.manufacturer = manu
+        self.iMax = float(iMax)
+        self.weight = float(weight)
+        
+#A class that defines an electric motor.
+class Motor:
+
+    #Initialization of the class from properties
+    def __init__(self, name, manu, Kv, gearRatio, noLoadCurrent, resistance, weight):
+
+        #Initialize members from constructor inputs
+        self.Kv = float(Kv)
+        self.Gr = float(gearRatio)
+        self.I0 = float(noLoadCurrent)
+        self.R = float(resistance)
+        self.name = name
+        self.manufacturer = manu
+        self.weight = float(weight)
+
+#A class of propellers defined by database test files
+class Propeller:
+    
+    #Initializes the prop from properties
+    def __init__(self, name, manu, dia, pitch, coefs):
+
+        self.name = name
+        self.manufacturer = manu
+        self.diameter = float(dia)
+        self.pitch = float(pitch)
+        self.thrustFitOrder = int(coefs[0])
+        self.fitOfThrustFitOrder = int(coefs[1])
+        self.powerFitOrder = int(coefs[2])
+        self.fitOfPowerFitOrder = int(coefs[3])
+
+        numThrustCoefs = (self.thrustFitOrder+1)*(self.fitOfThrustFitOrder+1)
+        self.thrustCoefs = coefs[4:numThrustCoefs+4].reshape((self.thrustFitOrder+1,self.fitOfThrustFitOrder+1)).astype(np.float)
+        self.powerCoefs = coefs[numThrustCoefs+4:].reshape((self.powerFitOrder+1,self.fitOfPowerFitOrder+1)).astype(np.float)
+
+        #These parameters will be set by later functions
+        self.vInf = 0.0
+        self.angVel = 0.0
+
+        if(False):
+            self.PlotCoefs()
+
+    def CalcTorqueCoef(self):
+        self.rpm = toRPM(self.angVel)
+        self.rps = self.rpm/60
+        self.J = self.vInf/(self.rps*self.diameter/12)
+        a = fit.poly_func(self.powerCoefs.T, self.rpm)
+        if(a[-1]>0):#Quadratic coefficient should always be non-positive
+            a[-1] = 0
+        self.Cl = fit.poly_func(a, self.J)/2*np.pi
+        
+
+    def CalcThrustCoef(self):
+        self.rpm = toRPM(self.angVel)
+        self.rps = self.rpm/60
+        self.J = self.vInf/(self.rps*self.diameter/12)
+        a = fit.poly_func(self.thrustCoefs.T, self.rpm)
+        if(a[-1]>0):#Quadratic coefficient should always be non-positive
+            a[-1] = 0
+        self.Ct = fit.poly_func(a, self.J)
+
+    def PlotCoefs(self):
+        #Plot thrust and torque coefficients
+        rpms = np.linspace(0,35000,10)
+        Js = np.linspace(0,1.4,10)
+        fig = plt.figure(figsize=plt.figaspect(1.))
+        fig.suptitle(self.name)
+        ax = fig.add_subplot(1,2,1, projection='3d')
+
+        for rpm in rpms:
+            a = fit.poly_func(self.thrustCoefs.T, rpm)
+            if(a[-1]>0):#Quadratic coefficient should always be non-positive
+                a[-1] = 0
+            thrust = fit.poly_func(a, Js)
+            rpmForPlot = np.full(len(thrust),rpm)
+            ax.plot(Js,rpmForPlot,thrust, 'r-')
+
+        ax.set_title("Predicted Thrust")
+        ax.set_xlabel("Advance Ratio")
+        ax.set_ylabel("RPM")
+        ax.set_zlabel("Thrust Coefficient")
+
+        ax = fig.add_subplot(1,2,2, projection='3d')
+
+        for rpm in rpms:
+            a = fit.poly_func(self.powerCoefs.T, rpm)
+            if(a[-1]>0):#Quadratic coefficient should always be non-positive
+                a[-1] = 0
+            power = fit.poly_func(a, Js)
+            rpmForPlot = np.full(len(power),rpm)
+            ax.plot(Js,rpmForPlot,power, 'r-')
+
+        ax.set_title("Predicted Power")
+        ax.set_xlabel("Advance Ratio")
+        ax.set_ylabel("RPM")
+        ax.set_zlabel("Power Coefficient")
+        plt.show()
+
+#A class that defines an entire electric propulsion unit
+class PropulsionUnit:
+
+    #Initialize the class from subclasses which are previously initialized
+    def __init__(self, prop, motor, battery, esc, altitude):
+        
+        self.prop = prop
+        self.motor = motor
+        self.batt = battery
+        self.esc = esc
+
+        _,_,_,self.airDensity = coesa.table(altitude*0.3048)
+        self.airDensity = self.airDensity*0.0019403203 # Converts kg/m^3 to slug/ft^3
+        
+        #Initialize exterior parameters to be set later
+        self.prop.vInf = 0
+        self.prop.angVel = 0
+        self.Im = 0 #Instantaneous current being drawn through the motor
+
+    #Computes motor torque (ft*lbf) given throttle setting and revolutions (rpm)
+    def CalcTorque(self, throttle, revs):
+        etaS = 1 - 0.078*(1 - throttle)
+        self.Im = (etaS*throttle*self.batt.V0 - (self.motor.Gr/self.motor.Kv)*revs)/(etaS*throttle*self.batt.R + self.esc.R + self.motor.R)
+        # Note: the 7.0432 constant converts units [(Nm/ftlb)(min/s)(rad/rev)]^-1
+        return 7.0432*self.motor.Gr/self.motor.Kv * (self.Im - self.motor.I0)
+    
+    #Computes thrust produced at a given cruise speed and throttle setting
+    def CalcCruiseThrust(self, cruiseSpeed, throttle):
+        if cruiseSpeed == 0 and throttle == 0:
+            self.prop.angVel = 0
+            return 0 #Don't even bother
+
+        self.prop.vInf = cruiseSpeed
+
+        #Determine the shaft angular velocity at which the motor torque and propeller torque are matched
+        #Uses a secant method
+        errorBound = 0.000001
+        approxError = 1 + errorBound #So that it executes at least once
+        w0 = 300 #An initial guess of the prop's angular velocity
+        self.prop.angVel = w0
+        self.prop.CalcTorqueCoef()
+        f0 = self.CalcTorque(throttle, toRPM(w0)) - self.prop.Cl*self.airDensity*(w0/(2*np.pi))**2*(self.prop.diameter/12)**5
+        w1 = w0 * 1.1
+        iterations = 0
+        
+        while approxError >= errorBound and iterations < 1000:
+            iterations = iterations + 1
+            self.prop.angVel = w1
+            self.prop.CalcTorqueCoef()
+            motorTorque = self.CalcTorque(throttle, toRPM(w1))
+            propTorque = self.prop.Cl*self.airDensity*(w1/(2*np.pi))**2*(self.prop.diameter/12)**5
+            f1 = motorTorque - propTorque
+            
+            w2 = w1 - (f1*(w0 - w1))/(f0 - f1)
+            if w2 < 0: # Prop angular velocity will never be negative even if windmilling
+                w2 = 0.01
+            elif w2 > self.motor.Kv*self.batt.V0: #Theoretically the upper limit
+                w2 = w2/3;
+
+            approxError = abs((w2 - w1)/w2)
+            
+            w0 = w1
+            f0 = f1
+            w1 = w2
+    
+#        if iterations == 1000:
+#            w = np.linspace(0,10000,1000)
+#            f = np.zeros(1000)
+#            for i in range(1000):
+#                self.prop.angVel = w[i]
+#                self.prop.CalcTorqueCoef()
+#                f[i] = self.CalcTorque(throttle, toRPM(w[i])) - self.prop.Cl*self.airDensity*(w[i]/(2*np.pi))**2*(self.prop.diameter/12)**5
+#            plt.plot(w,f)
+#            plt.title("Torque Balance vs Angular Velocity")
+#            plt.show()
+        
+        self.prop.angVel = w2
+        self.prop.CalcThrustCoef()
+        _ = self.CalcTorque(throttle, toRPM(w2)) # To make sure member variables are fully updated
+
+        return self.prop.Ct*self.airDensity*(w2/(2*np.pi))**2*(self.prop.diameter/12)**4
+    
+    #Computes required throttle setting for a given thrust and cruise speed
+    def CalcCruiseThrottle(self, cruiseSpeed, reqThrust):
+        #Uses a secant method
+        errorBound = 0.000001
+        approxError = 1 + errorBound
+        t0 = 0.5
+        T0 = self.CalcCruiseThrust(cruiseSpeed, t0)
+        t1 = t0*1.1
+        iterations = 0
+        
+        while approxError >= errorBound and iterations < 1000:
+            
+            iterations = iterations + 1
+            T1 = self.CalcCruiseThrust(cruiseSpeed, t1) - reqThrust
+            
+            t2 = t1 - (T1*(t0 - t1))/(T0 - T1)
+            
+            approxError = abs((t2 - t1)/t2)
+            
+            if t2 > 10:
+                t2 = 1.1
+            elif t2 < -10:
+                t2 = -0.1
+            t0 = t1
+            T0 = T1
+            t1 = t2
+
+        #if iterations == 1000:
+        #    t = np.linspace(0,1.0,100)
+        #    T = np.zeros(100)
+        #    for i in range(100):
+        #        T[i] = self.CalcCruiseThrust(cruiseSpeed, t[i]) - reqThrust
+        #    plt.plot(t,T) 
+        #    plt.show()
+
+        if t2 > 1 or t2 < 0:
+            return None
+        
+        self.CalcCruiseThrust(cruiseSpeed,t2) # To make sure member variables are fully updated
+        return t2
+        
+    #Plots thrust curves for propulsion unit up to a specified airspeed
+    def PlotThrustCurves(self, maxAirspeed, numVels, numThrSets):
+        
+        vel = np.linspace(0, maxAirspeed, numVels)
+        thr = np.linspace(0, 1, numThrSets)
+        thrust = np.zeros((numVels, numThrSets))
+        rpm = np.zeros((numVels,numThrSets))
+        
+        for i in range(numVels):
+            for j in range(numThrSets):
+                
+                #print("Freestream Velocity: ", vel[i])
+                #print("Throttle Setting: ", thr[j])
+                thrust[i][j] = self.CalcCruiseThrust(vel[i], thr[j])
+                rpm[i][j] = toRPM(self.prop.angVel)
+
+        fig = plt.figure()
+        fig.suptitle("Components: " + str(self.prop.name) + ", " + str(self.motor.name) + ", and " + str(self.batt.name))
+
+        ax0 = fig.add_subplot(1,2,1)
+        for i in range(numVels):
+            ax0.plot(thr, thrust[i])
+        ax0.set_title("Thrust")
+        ax0.set_ylabel("Thrust [lbf]")
+        ax0.set_xlabel("Throttle Setting")
+        ax0.legend(list(vel), title="Airspeed [ft/s]")
+
+        ax1 = fig.add_subplot(1,2,2)
+        for i in range(numVels):
+            ax1.plot(thr, rpm[i])
+        ax1.set_title("Prop Speed")
+        ax1.set_ylabel("Speed [rpms]")
+        ax1.set_xlabel("Throttle Setting")
+        plt.show()
+
+    #Determines how long the battery will last based on a required thrust and cruise speed
+    def CalcBattLife(self, cruiseSpeed, reqThrust):
+        throttle = self.CalcCruiseThrottle(cruiseSpeed, reqThrust)
+        if(throttle==None or self.Im > self.esc.iMax or self.Im > self.batt.iMax):
+            return None
+        #print("Throttle Setting:",throttle)
+        #print("Current Draw:",self.Im)
+        runTime = (self.batt.cellCap/1000)/self.Im*60 # Gives run time in minutes, assuming nominal cell capacity and constant battery votlage
+        if runTime < 0:
+            return None
+        return runTime
+
+    def GetWeight(self):#Returns weight of electrical components in pounds
+        return (self.batt.weight + self.motor.weight + self.esc.weight)/16
